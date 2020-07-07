49f4a70c1b2b57046b300fcc68e42d582da1691b
Arpit Malaiya <arpitmalaiya@gmail.com>
2020-07-07 23:30:50 +0530

    second

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/myapp'));
+app.use(express.static(__dirname + '/dist/my-app'));
 app.get('/*', function (req, res) {
     res.sendFile(path.join(__dirname +
-        '/dist/myapp/index.html'));
+        '/dist/my-app/index.html'));
 });
 app.listen(process.env.PORT || 8080);
